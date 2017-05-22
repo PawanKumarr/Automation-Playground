dc3e5763a61e28acf5835f717ca99fe8c5e0cea2
Pawan Kumar <pawan.mail.id@gmail.com>
2017-05-22 21:52:02 +0530

    Created Test Fixture using @BeforeClass

--- JUnitBasics/src/com/pawan/test/CalculatorTest.java
+++ JUnitBasics/src/com/pawan/test/CalculatorTest.java
@@ -2,22 +2,29 @@ package com.pawan.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.pawan.Calculator;
 
 public class CalculatorTest {
 
+	private static Calculator calc;
+	
+	@BeforeClass 
+	public static void setUpClass(){
+		calc = new Calculator();
+	}
+	
 	@Test
 	public void testAdd() {
-		Calculator calc = new Calculator();
 		int result = calc.add(1, 2);
 		assertEquals(3, result);
 	}
 
 	@Test
 	public void testMultiply() {
-		Calculator calc = new Calculator();
 		int result = calc.multiply(1, 2);
 		assertEquals(2, result);
 	}
